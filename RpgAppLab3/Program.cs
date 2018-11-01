9dd51538e13b9de20c8ddacd664fdc7e220f61fc
Nils Nyman <nils.nyman@gmail.com>
2018-11-01 14:03:27 +0100

    Added a d10 roll

--- RpgAppLab3/Program.cs
+++ RpgAppLab3/Program.cs
@@ -9,6 +9,11 @@ namespace RpgAppLab3
             var rnd = new Random();
             Console.WriteLine("What do you need?");
             var command = Console.ReadLine();
+            if (command == "roll d10")
+            {
+                int diceRoll = rnd.Next(1, 10 + 1);
+                Console.WriteLine($"Rolled a D10: {diceRoll}");
+            }
             if (command == "names")
             {
                 var allNames = new[] { "Briana", "Ben", "David",
