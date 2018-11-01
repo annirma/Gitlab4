c91caae85dbd39aa833da46bb81abd43ea8c3428
Fredrik Jäderland <fredrikjaderland@hotmail.com>
2018-11-01 14:10:25 +0100

    Added D4 functionality

--- RpgAppLab3/Program.cs
+++ RpgAppLab3/Program.cs
@@ -16,6 +16,10 @@ namespace RpgAppLab3
                 var randomIndex = rnd.Next(allNames.Length);
                 Console.WriteLine(allNames[randomIndex]);
             }
+            else if(command == "!roll D4")
+            {
+                Console.WriteLine($"You rolled a {rnd.Next(1, 4 + 1)}");
+            }
             Console.ReadKey();
         }
     }
