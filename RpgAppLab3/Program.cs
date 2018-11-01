d0822b7967f5c685d8aa497e77ad317620581090
Joshua Anthony <joshua.anthony@habanero.se>
2018-11-01 09:57:03 +0100

    Added initial funcitonality

--- RpgAppLab3/Program.cs
+++ RpgAppLab3/Program.cs
@@ -6,7 +6,17 @@ namespace RpgAppLab3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rnd = new Random();
+            Console.WriteLine("What do you need?");
+            var command = Console.ReadLine();
+            if (command == "names")
+            {
+                var allNames = new[] { "Briana", "Ben", "David",
+            "Dana", "Felicia", "Felix" };
+                var randomIndex = rnd.Next(allNames.Length);
+                Console.WriteLine(allNames[randomIndex]);
+            }
+            Console.ReadKey();
         }
     }
 }
